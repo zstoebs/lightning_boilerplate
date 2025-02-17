ea29cce9d8a8f431bdadef43542e9c3bfe6b0465
zstoebs <zstoebner25@gmail.com>
2025-02-17 17:57:57 -0600

    debug model template

--- models/__init__.py
+++ models/__init__.py
@@ -5,21 +5,22 @@ import lightning.pytorch as pl
 class ABCModel(ABC, pl.LightningModule):
 	def __init__(self):
 		super().__init__()
-		self.outputs = []
-		self.scores = None
+		self.outputs = None # for storing model outputs
+		self.scores = None # for storing metric evals
+
   
 	def on_validation_start(self):
-		self.outputs = []
+		self.outputs = None
 		self.scores = None
 		super().on_validation_start()
 	
 	def on_test_start(self):
-		self.outputs = []
+		self.outputs = None
 		self.scores = None
 		super().on_test_start()
   
 	def on_predict_start(self) -> None:
-		self.outputs = []
+		self.outputs = None
 		self.scores = None
 		super().on_predict_start()
 		
