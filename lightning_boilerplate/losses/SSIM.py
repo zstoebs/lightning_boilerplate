b567fa22032ce7fa3fa3805104c8cac93bb56db1
zstoebs <zstoebner25@gmail.com>
2025-10-20 15:55:36 -0500

    ssim dev

--- lightning_boilerplate/losses/SSIM.py
+++ lightning_boilerplate/losses/SSIM.py
@@ -4,6 +4,6 @@ from typing import Callable
 from torchmetrics.image.ssim import StructuralSimilarityIndexMeasure
 
 def ssim(pred: Tensor, target: Tensor, data_range: float=1., reduction: Callable | None=pt.mean, **kwargs) -> Tensor:
-    ssim_metric = StructuralSimilarityIndexMeasure(data_range=data_range, **kwargs)
+    ssim_metric = StructuralSimilarityIndexMeasure(data_range=data_range, **kwargs).to(pred.device)
     ssim_value = ssim_metric(pred, target)
     return reduction(ssim_value) if reduction is not None else ssim_value
